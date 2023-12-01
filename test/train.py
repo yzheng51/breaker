c69838201505db9cb40f60dc0bd07f41e25d8071
yzheng51 <43605323+yzheng51@users.noreply.github.com>
2023-12-01 10:10:59 +0800

    update train.py

--- test/train.py
+++ test/train.py
@@ -14,7 +14,6 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
 from utils.loss import kl_divergence_loss
-from utils.evaluate import uauc_score
 from utils.dataset import MyDataset
 from utils.model import UserRepresentation, Breaker
 
@@ -103,28 +102,29 @@ print(train.columns[:5])
 print(train.columns[-5:])
 
 # =========================================================================================
-# Create dataset for computing GAUC
+# Create dataset for computing AER
 # =========================================================================================
-uauc_valid = valid.loc[valid[label_col] > 0].reset_index(drop=True).copy()
-print(f"uauc_valid.shape={uauc_valid.shape}")
+aer_valid = valid
+aer_valid_bak = valid.copy()
+print(f"aer_valid.shape={aer_valid.shape}")
 
-to_valid = uauc_valid[["uid", "dt"] + feats].drop_duplicates().reset_index(drop=True)
+to_valid = aer_valid[["uid", "dt"] + feats].drop_duplicates().reset_index(drop=True)
 to_valid["item"] = ",".join(map(str, candidates))
 to_valid["item"] = to_valid["item"].str.split(",")
 
 to_valid = to_valid.explode("item")
 to_valid["item"] = to_valid["item"].astype("int32")
 
-tmp = uauc_valid[["uid", "item", "dt", label_col]]
+tmp = aer_valid[["uid", "item", "dt", label_col]]
 
 to_valid = to_valid.merge(tmp, on=["uid", "item", "dt"], how="left")
 to_valid[label_col].fillna(0, inplace=True)
 to_valid[label_col] = to_valid[label_col].astype("int8")
 
-uauc_valid = to_valid.copy()
+aer_valid = to_valid.copy()
 del to_valid, tmp
 
-print(f"uauc_valid.shape={uauc_valid.shape}")
+print(f"aer_valid.shape={aer_valid.shape}")
 
 # =========================================================================================
 # Negative sampling
@@ -154,14 +154,14 @@ feat_map is a mapping from a user feat to its unique count which looks like
 
 train["item"] = train["item"].map(item_mapping)
 valid["item"] = valid["item"].map(item_mapping)
-uauc_valid["item"] = uauc_valid["item"].map(item_mapping)
+aer_valid["item"] = aer_valid["item"].map(item_mapping)
 
 # cumsum for each feat to ensure it can be embedded in one class
 counter = 0
 for f in feats:
     train[f] = train[f].values + counter
     valid[f] = valid[f].values + counter
-    uauc_valid[f] = uauc_valid[f].values + counter
+    aer_valid[f] = aer_valid[f].values + counter
 
     counter += feat_map[f]
 
@@ -169,12 +169,12 @@ print(f"counter={counter}")
 
 train_user_feat = train[feats].values
 valid_user_feat = valid[feats].values
-uauc_valid_user_feat = uauc_valid[feats].values
+aer_valid_user_feat = aer_valid[feats].values
 
 print(
     f"train_user_feat.shape={train_user_feat.shape}, "
     f"valid_user_feat.shape={valid_user_feat.shape}, "
-    f"uauc_valid_user_feat.shape={uauc_valid_user_feat.shape}"
+    f"aer_valid_user_feat.shape={aer_valid_user_feat.shape}"
 )
 
 y_train = train[[label_col]].values
@@ -182,13 +182,13 @@ y_valid = valid[[label_col]].values
 
 train_data = MyDataset(train_user_feat, train[["item"]].values, y_train)
 valid_data = MyDataset(valid_user_feat, valid[["item"]].values, y_valid)
-uauc_valid_data = MyDataset(uauc_valid_user_feat, uauc_valid[["item"]].values, uauc_valid[[label_col]].values)
+aer_valid_data = MyDataset(aer_valid_user_feat, aer_valid[["item"]].values, aer_valid[[label_col]].values)
 
 train_loader = torch.utils.data.DataLoader(
     train_data,
     shuffle=True,
     drop_last=True,
-    batch_size=2048 * 2,
+    batch_size=2048,
     num_workers=2
 )
 valid_loader = torch.utils.data.DataLoader(
@@ -198,8 +198,8 @@ valid_loader = torch.utils.data.DataLoader(
     batch_size=2048 * 4,
     num_workers=2
 )
-uauc_valid_loader = torch.utils.data.DataLoader(
-    uauc_valid_data,
+aer_valid_loader = torch.utils.data.DataLoader(
+    aer_valid_data,
     shuffle=False,
     drop_last=False,
     batch_size=2048 * 4,
@@ -209,9 +209,15 @@ uauc_valid_loader = torch.utils.data.DataLoader(
 # =========================================================================================
 # Train Breaker
 # =========================================================================================
-epochs = 20
+max_epochs = 20
 n_clusters = 4
-embed_size = 5
+embed_size = 10
+steps_per_epoch = len(train_loader)
+target_cycle = int(steps_per_epoch * 0.1)
+# early stop strategy
+earlystop_patience = 1
+best_aer = -999
+earlystop_counter = 0
 # user representation
 init_user_embed = UserRepresentation(
     n_feat=counter,
@@ -247,9 +253,9 @@ criterion_kl = kl_divergence_loss
 criterion_log = torch.nn.BCELoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
 
-print("|  Epoch  |     Step     |   Score    |    GAUC    |  Recall@1  | Epapsed Time |")
-print("| ------- | ------------ | ---------- | ---------- | ---------- | ------------ |")
-for e in range(epochs):
+print("|  Epoch  |     Step     |   Score    |     AER    | Epapsed Time |")
+print("| ------- | ------------ | ---------- | ---------- | ------------ |")
+for e in range(max_epochs):
     start = time.perf_counter()
     train_kl_losses = list()
     train_log_losses = list()
@@ -278,7 +284,7 @@ for e in range(epochs):
         train_kl_losses.append(kl_loss.item())
         train_log_losses.append(log_loss.item())
 
-        if (ii + 1) % 100 == 0:
+        if (ii + 1) % target_cycle == 0:
             net_for_p.load_state_dict(net.state_dict())
             print(np.mean(train_kl_losses))
             print(np.mean(train_log_losses))
@@ -295,36 +301,44 @@ for e in range(epochs):
     test_score = np.hstack(test_score)
 
     net.eval()
-    test_score_uauc = list()
-    for u_feat, i_feat, labels in uauc_valid_loader:
+    test_score_aer = list()
+    for u_feat, i_feat, labels in aer_valid_loader:
         u_feat, i_feat, labels = u_feat.long().to(device), i_feat.long().to(device), labels.float().to(device)
         with torch.no_grad():
             output_q, output_target = net(u_feat, i_feat)
 
-        test_score_uauc.append(output_target.cpu().numpy())
-    test_score_uauc = np.hstack(test_score_uauc)
+        test_score_aer.append(output_target.cpu().numpy())
+    test_score_aer = np.hstack(test_score_aer)
 
     # output train information
-    uauc = uauc_score(uauc_valid[label_col], test_score_uauc.ravel(), uauc_valid["uid"].values)
-    uauc_valid["score"] = test_score_uauc.ravel()
-    recall_at_1 = uauc_valid.sort_values(["uid", "score", "item"], ascending=False).reset_index(drop=True)
-    recall_at_1["item"] = recall_at_1["item"].map(dict(zip(list(item_mapping.values()), list(item_mapping.keys()))))
-    print(recall_at_1.groupby("uid").head(1)["item"].value_counts())
-    recall_at_1 = recall_at_1.groupby("uid").head(1)[label_col].mean()
+    aer_valid["score"] = test_score_aer.ravel()
+    aer_cal_df = aer_valid.sort_values(by=["uid","dt","score"], ascending=[True, True, False]).groupby(['uid', 'dt']).first().reset_index(drop=True)
+    aer_cal_df["item"] = aer_cal_df["item"].map(dict(zip(list(item_mapping.values()), list(item_mapping.keys()))))
+    aer_cal_df = pd.merge(aer_cal_df[['uid','dt','item','score']], aer_valid_bak[['uid','dt','item',label_col]], how='inner')
+    aer_score = np.mean(aer_cal_df.loc[:, label_col])
+
     print(output_q.mean(0))
     print(pd.Series(output_q.argmax(1).cpu().numpy()).value_counts())
     duration = time.perf_counter() - start
     print(
-        "|  {}/{} ".format(e + 1, epochs).ljust(9),
+        "|  {}/{} ".format(e + 1, max_epochs).ljust(9),
         "|  {}/{} ".format(ii, len(train_loader)).ljust(14),
         "|   {:.4f}  ".format(roc_auc_score(y_valid, test_score)).ljust(12),
-        "|   {:.4f}  ".format(uauc).ljust(12),
-        "|   {:.4f}  ".format(recall_at_1).ljust(12),
+        "|   {:.4f}  ".format(aer_score).ljust(12),
         "|  {:.2f}s ".format(duration).ljust(14),
         "|"
     )
-    model_path = f"../models/breaker/{e+1}.pth"
-    torch.save(net.state_dict(), model_path)
+
+    if aer_score > best_aer:
+        print(f'Validation AER increased ({best_aer:.6f} --> {aer_score:.6f}).  Saving model ...')
+        best_aer=aer_score
+        model_path = f"../models/breaker/{e+1}.pth"
+        torch.save(net.state_dict(), model_path)
+    else:
+        earlystop_counter += 1
+        if earlystop_counter >= earlystop_patience:
+            print(f'EarlyStopping counter: {earlystop_counter} out of {earlystop_patience};')
+            break
 
 # =========================================================================================
 # Visualization
